0c2268b5eec7bd037dc73e3280f65b87a06057a6
alecdalelio <alecdalelio@gmail.com>
2021-01-14 16:14:59 -0500

    cut down webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,17 +10,3 @@ module.exports = {
     contentBase: path.resolve(__dirname, "./dist"),
   },
 };
-
-module.exports = {
-  // 1
-  entry: "./src/index.js",
-  // 2
-  output: {
-    path: "/dist",
-    filename: "bundle.js",
-  },
-  // 3
-  devServer: {
-    contentBase: "./dist",
-  },
-};
